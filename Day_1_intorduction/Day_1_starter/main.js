f7b5c7f6b99146f79df038c67427d021f040b0e7
kathrinesathi <kathrinesathi@gmail.com>
2020-10-15 20:31:46 +0530

    introduction

---
+++ Day_1_intorduction/Day_1_starter/main.js
@@ -0,0 +1,18 @@
+// declaring different variables of different data types
+
+let firstName = 'Kathrine';
+let lastName = 'Sathi';
+let country = 'India';
+let city = 'Chennai';
+let age = 20;
+let isgraduated = true;
+
+// declaring variables with number values
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+// variables can also be declaring in one line separated by comma
+
+let name ='kathrine' , role='intern', live='India';
---
+++ Day_1_intorduction/Day_1_starter/variable.js
@@ -0,0 +1,5 @@
+// the variable values can be accessed from other variable.js file
+
+console.log(firstName, lastName, country, city, age, isgraduated);
+console.log(gravity, boilingPoint, PI);
+console.log(name, role, live);
