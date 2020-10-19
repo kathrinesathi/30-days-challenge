5ae5a52c109d1b97426d7b210065c4d0d8d41fd3
kathrinesathi <kathrinesathi@gmail.com>
2020-10-19 23:37:11 +0530

    for of

--- Day_3_conditional_loops/for.js
+++ Day_3_conditional_loops/for.js
@@ -1,3 +1,22 @@
 for (var i = 0; i <= 10; i++) {
     console.log(i)
 }
+var text = " "
+var person = {
+    fname: "ced",
+    lname: "kathy",
+    age: 20
+};
+var x
+for (x in person) {
+    text += person[x] + " "
+}
+document.getElementById("loop").innerHTML = text;
+
+//for of statement loops through the values of the array
+
+var colours = ["blue", "green", "Yellow"];
+var x
+for (x of colours) {
+    document.write(x + "<br>")
+}
