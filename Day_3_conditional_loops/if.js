32a03b0b80647f5b67aa2c2efe0194b8bd7f8ccd
kathrinesathi <kathrinesathi@gmail.com>
2020-10-19 22:08:41 +0530

    conditional

---
+++ Day_3_conditional_loops/if.js
@@ -0,0 +1,8 @@
+const name = prompt('what is your name')
+if (name == 'kat' || name == 'yoga') {
+    alert("hello")
+} else if (name == 'hari') {
+    console.log('this is Hari')
+} else {
+    console.log('Hii....')
+}
