8fc7c9dd03c415c61c3797e2f421962c9602d349
kathrinesathi <kathrinesathi@gmail.com>
2020-10-22 23:34:50 +0530

    variable

---
+++ Day_6_variables/arr.js
@@ -0,0 +1,13 @@
+const web = ['HTML','CSS','JS','React','PHP','MYSQL']
+
+// creates an array and fill with static elements
+const four = Array(4).fill(4)
+console.log(four)
+
+// concat
+const pro =['python', 'c']
+const cse = web.concat(pro)
+console.log(cse)
+
+//getting an element using index
+console.log(cse.indexOf('python'))
