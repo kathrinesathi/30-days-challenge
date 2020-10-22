899781732cca252d31792021ef545760f05681f9
kathrinesathi <kathrinesathi@gmail.com>
2020-10-22 23:20:33 +0530

    functions

--- Day_5_Functions/functions.js
+++ Day_5_Functions/functions.js
@@ -44,3 +44,12 @@ function square() {
   console.log(sumAllNums(1, 2, 3, 4)) // 10
   console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
   console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
+
+  // function with default parameters
+  const greetings = (name = 'Ced') => {
+    let message = name + ', welcome to 30 Days Of JavaScript!'
+    return message
+  }
+  
+  console.log(greetings())
+  console.log(greetings('Kat'))
