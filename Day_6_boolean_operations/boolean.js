7e128872e54e5f9f6d6ea78f4ffb352d73a6aeb0
kathrinesathi <kathrinesathi@gmail.com>
2020-10-23 21:22:29 +0530

    boolean, date, operators

---
+++ Day_6_boolean_operations/boolean.js
@@ -0,0 +1,12 @@
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isgraduated = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+
+let firstName
+console.log(firstName) //not defined, because it is not assigned to a value yet
+
+let empty = null
+console.log(empty) // -> null , means no value
