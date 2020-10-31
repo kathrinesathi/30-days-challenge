d610a0bb3409533763b2a652c73b93f277d6a5d5
kathrinesathi <kathrinesathi@gmail.com>
2020-10-31 22:04:54 +0530

    events

---
+++ Day_12_Regular_expressions/events.js
@@ -0,0 +1,24 @@
+function hithere(){
+    alert('Hi there')
+}
+function changeBackground(){
+    var x = document.getElementById('bg')
+    bg.style.backgroundColor ='blue';
+}
+
+function hov(){
+    var e = document.getElementById('');
+    e.style.display ='none';
+}
+
+function out() {
+    var e = document.getElementById('');
+    e.style.display ='none';
+}
+
+function focused(){
+    var e = document.getElementById('')
+    if (confirm('Got it?')){
+        e.blur();
+    }
+}
